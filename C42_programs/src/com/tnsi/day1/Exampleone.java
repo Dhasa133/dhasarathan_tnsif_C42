ae845f28af6d633b3a8a3b8db3961a82378dde2f
dhasa <dhasa@DESKTOP-35A384R>
2024-08-14 10:52:24 +0530

    commit 1

---
+++ C42_programs/src/com/tnsi/day1/Exampleone.java
@@ -0,0 +1,10 @@
+package com.tnsi.day1;
+
+public class Exampleone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ C42_programs/src/com/tnsif/day2/EncapsulationDemo.java
@@ -0,0 +1,31 @@
+package com.tnsif.day2;
+
+public class EncapsulationDemo {
+	String playerName;
+	int jersyNo;
+	int orderNo;
+	public String getPlayerName() {
+		return playerName;
+	}
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+	public int getJersyNo() {
+		return jersyNo;
+	}
+	public void setJersyNo(int jersyNo) {
+		this.jersyNo = jersyNo;
+	}
+	public int getOrderNo() {
+		return orderNo;
+	}
+	public void setOrderNo(int orderNo) {
+		this.orderNo = orderNo;
+	}
+	public static void main(String []args) {
+		
+		
+	}
+	
+
+}
