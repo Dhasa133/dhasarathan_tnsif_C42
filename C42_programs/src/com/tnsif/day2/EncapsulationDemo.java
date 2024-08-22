a40be78feaaf799b7c88e2e5bd2b10fd99b3a9e1
dhasa <dhasa@DESKTOP-35A384R>
2024-08-22 10:07:06 +0530

    gdgf

--- C42_programs/src/com/tnsif/day2/EncapsulationDemo.java
+++ C42_programs/src/com/tnsif/day2/EncapsulationDemo.java
@@ -1,31 +1,52 @@
 package com.tnsif.day2;
 
+
+
 public class EncapsulationDemo {
-	String playerName;
-	int jersyNo;
-	int orderNo;
+	//Team RCB
+	
+	String playerName ;
+	int orderNo ;
+	int jersyNo ;
+
 	public String getPlayerName() {
 		return playerName;
 	}
+
 	public void setPlayerName(String playerName) {
 		this.playerName = playerName;
 	}
-	public int getJersyNo() {
-		return jersyNo;
-	}
-	public void setJersyNo(int jersyNo) {
-		this.jersyNo = jersyNo;
-	}
+
 	public int getOrderNo() {
 		return orderNo;
 	}
+
 	public void setOrderNo(int orderNo) {
-		this.orderNo = orderNo;
+		this.orderNo = orderNo ;
 	}
-	public static void main(String []args) {
-		
-		
+
+	public int getJersyNo() {
+		return jersyNo;
+	}
+
+	public void setJersyNo(int jersyNo) {
+		this.jersyNo = jersyNo;
 	}
 	
+
+	@Override
+	public String toString() {
+		return "EncapsulationDemo [playerName = " + playerName + ", orderNo = " + orderNo + ", jersyNo = " + jersyNo + "]";
+	}
+
+	public static void main(String[] args) {
+		EncapsulationDemo obj = new EncapsulationDemo();
+		
+		obj.setPlayerName("Virat kohli");
+		obj.setJersyNo(18);
+		obj.setOrderNo(2);
+		
+		System.out.println(obj);
+	}
 
 }
